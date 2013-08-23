5504e2a3aee21426a9a846ffeaad2fb2549ad2de
Achal Shah <achals@gmail.com>
2013-08-23 01:11:42 -0700

    Adding GET

--- src/com/achals/lobster/HttpEngine/EngineMain.scala
+++ src/com/achals/lobster/HttpEngine/EngineMain.scala
@@ -3,8 +3,10 @@ package com.achals.lobster.HttpEngine
 object EngineMain {
   def main(args: Array[String]) = {
     val engine = new HttpEngine()
-    val contents = engine.HEAD("http://www.google.com:80")
-    println(contents.get.Body)
-    ()
+    val contents = engine.GET("http://www.google.com:80")
+    contents match {
+      case Some(ans) => println(ans.Headers)
+      case None      => println("Error in performing action.")
+    }
   }
 }
--- src/com/achals/lobster/HttpEngine/HttpEngine.scala
+++ src/com/achals/lobster/HttpEngine/HttpEngine.scala
@@ -1,7 +1,8 @@
 package com.achals.lobster.HttpEngine
 
 import java.net.{URL, Socket}
-import java.io.{BufferedReader, InputStreamReader, InputStream};
+import java.io.{BufferedReader, InputStreamReader, InputStream, OutputStream};
+import java.lang.Integer;
 import scala.annotation.tailrec
 
 class HttpEngine {
@@ -13,17 +14,38 @@ class HttpEngine {
     val outStream = sock.getOutputStream()
     val inStream  = sock.getInputStream()
     val inBufReader = new BufferedReader(new InputStreamReader(inStream));
+    makeRequest(outStream, "HEAD", urlObj)
+    parseHeaders(url, inBufReader)
+  }
+  
+  def GET(url:String) : Option[Contents] = {
+    val urlObj = new URL(url)
+    val sock = new Socket(urlObj.getHost(), urlObj.getPort())
     
+    val outStream = sock.getOutputStream()
+    val inStream  = sock.getInputStream()
+    val inBufReader = new BufferedReader(new InputStreamReader(inStream));
+  
+    makeRequest(outStream, "GET", urlObj)
+    val headers = parseHeaders(url, inBufReader)
+    println(headers.get.Headers)
+    println(headers.get.Body)
+    val output = headers match {
+      case Some(content) => parseBody(inBufReader, content)
+      case None 		 => None
+    }
+    output
+  }
+  
+  private def makeRequest(outStream: OutputStream, command: String, urlObj:URL):Unit = {
     val file = urlObj.getFile()
     val host = urlObj.getHost()
-    
-    outStream.write(("HEAD /"+file+" HTTP/1.1\r\n").getBytes());
+
+    outStream.write((command+" /"+file+" HTTP/1.1\r\n").getBytes());
     outStream.write(("User-Agent: Lobstercrawler\r\n").getBytes());
     outStream.write(("Connection: close\r\n").getBytes());
     outStream.write(("Host: "+host+"\r\n\r\n").getBytes());
-    outStream.flush();         
-
-    parseHeaders(url, inBufReader)
+    outStream.flush();
   }
   
   private def parseHeaders(url:String, inStream: BufferedReader): Option[Contents] = {
@@ -35,6 +57,33 @@ class HttpEngine {
     else
       None
         
+  }
+  
+  private def parseBody(inBufReader: BufferedReader, tempContents: Contents): Option[Contents] = {
+		  tempContents.Headers.get("Transfer-Encoding") match {
+		    case Some("Chunked") => Some(getChunkedResponse(inBufReader, tempContents))
+		    case None 				 => None
+		  }
+		  None
+  }
+  
+  def getChunkedResponse(inBufReader:BufferedReader, tempContents:Contents): Contents = {
+    val line = inBufReader.readLine()
+    if (line==null || line == "0")
+      tempContents
+    else {
+      val size = Integer.parseInt(line, 16)
+      var bodyLine = ""
+      for (i <- 0 to size-1) {
+        bodyLine = line+inBufReader.read()
+      }
+      inBufReader.read();
+      inBufReader.read();
+      
+      getChunkedResponse(inBufReader, Contents(tempContents.URL,
+    		  								   tempContents.Headers,
+    		  								   tempContents.Body + bodyLine))
+    }
   }
   
   @tailrec
